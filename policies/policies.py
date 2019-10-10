0c01f32d4084a0cbaf8f0f4c6a33900ef337533a
dovedevic <dovedevic@gmail.com>
2019-10-10 16:59:27 -0400

    Refactored to make base replacement policy template, removed enumeration

--- policies/policies.py
+++ policies/policies.py
@@ -1,21 +1,8 @@
-from enum import Enum
 
-
-class ReplacementPolicies(Enum):
-    LRU = 0
-    NMRU = 0
-    RANDOM = 0
-    LFU = 0
-    NMFU = 0
-
-
-class ReplacementPolicy:
-    def __init__(self, policy):
-        self.policy = policy
-        if policy == 0:
-            raise NotImplementedError("Cache replacement policy is not implemented!")
-
+class BaseReplacementPolicy:
+    def __init__(self):
         # TODO::Implement
+        self._clock = 0
 
     @staticmethod
     def name():
@@ -26,7 +13,13 @@ class ReplacementPolicy:
         return 0
 
     def touch(self, block):
-        pass
+        return 0
 
     def evict(self, cache_set):
-        pass
+        return None
+
+    def step(self):
+        self._clock += 1
+
+
+
