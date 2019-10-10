35107d99a988a2db2d14fa1d6afa0472a546aa72
Alexandar Devic <dovedevic@gmail.com>
2019-10-10 04:16:31 -0400

    moved declaration of block, modified policies API

--- cache.py
+++ cache.py
@@ -3,8 +3,61 @@ import enum
 from policies import ReplacementPolicy
 
 
+class Block:
+    """
+    Defines the most atomic unit in a cache, the block
+    """
+
+    def __init__(self, base_address, dirty: bool, policy: ReplacementPolicy):
+        """
+        Initializer for the generic cache block
+        :param base_address: The tag and index of an address
+        :param dirty: The determination if this block was ever writen to or not
+        :param policy_data: The replacement policy metadata that keeps track of the block
+        """
+        self._base_address = base_address
+        self._dirty = dirty
+        self._policy = policy
+        self._policy_data = policy.default()
+
+    def __str__(self):
+        return "[{}]{},{}:{}".format(hex(self._base_address), self._dirty, self._policy.name(), self._policy_data)
+
+    def __eq__(self, other):
+        return True if isinstance(other, Block) and self._base_address == other._base_address else False
+
+    def read(self):
+        """
+        Perform a simulated read on this cache block
+        :return: nothing
+        """
+        self._policy_data = self._policy.touch(self)
+
+    def write(self):
+        """
+        Perform a simulated write on this cache block
+        :return: nothing
+        """
+        self._policy_data = self._policy.touch(self)
+        self._dirty = True
+
+    def is_dirty(self):
+        """
+        Returns if this block is written to, or dirty
+        :return: boolean, if this block is dirty
+        """
+        return self._dirty
+
+    def base_address(self):
+        """
+        Returns this blocks base address
+        :return: int, the base address
+        """
+        return self._base_address
+
+
 class Cache:
-    def __init__(self, addressspace, size, associativity, blocksize, policy):
+    def __init__(self, addressspace, size, associativity, blocksize, policy: ReplacementPolicy):
         self._addressspace = addressspace.value
         self._size = size
         self._associativity = associativity
@@ -38,70 +91,31 @@ class Cache:
         """
         return None
 
-    def put(self, address, data):
+    def put(self, block: Block):
         """
-        Put the following address block into the cache. If not space is present, use the policy to evict and return the eviction
-        :param address:
-        :param data
+        Put the following block into the cache. If not space is present, use the policy to evict and return the
+        eviction. If space is available or the block is present, place and return
+        :param block:
         :return replacement:
         """
 
-        cache_set = address << self._tag_bits >> self._tag_bits + self._offset_bits
-        temp_block = Block(address >> self._offset_bits << self._offset_bits, 0, 1, data)
-        if temp_block in self._cache[cache_set]:
-            # Putting the same block in cache
+        cache_set = block.base_address() << self._tag_bits >> self._tag_bits + self._offset_bits
+        if block in self._cache[cache_set]:
+            # Block is existing in cache, assuming rewrite
+            replacement = self._cache[cache_set].index(block)
+            self._cache[cache_set][replacement] = block
             return None
         elif None in self._cache[cache_set]:
+            # Space available in cache for new block, place it
             placement = self._cache[cache_set].index(None)
-            self._cache[cache_set][placement] = temp_block
+            self._cache[cache_set][placement] = block
             return None
         else:
-            pass
-
-
-class Block:
-    """
-    Defines the most atomic unit in a cache, the block
-    """
-
-    def __init__(self, base_address, dirty: bool, policy: ReplacementPolicy):
-        """
-        Initializer for the generic cache block
-        :param base_address: The tag and index of an address
-        :param dirty: The determination if this block was ever writen to or not
-        :param policy: The replacement policy that keeps track of this block
-        """
-        self._base_address = base_address
-        self._dirty = dirty
-        self._rep_policy = policy.default()
-
-    def __str__(self):
-        return "[{}]{},{}".format(hex(self._base_address), self._dirty, self._rep_policy.name())
-
-    def __eq__(self, other):
-        return True if isinstance(other, Block) and self._base_address == other._base_address else False
-
-    def read(self):
-        """
-        Perform a simulated read on this cache block
-        :return: nothing
-        """
-        self._rep_policy.touch()
-
-    def write(self):
-        """
-        Perform a simulated write on this cache block
-        :return: nothing
-        """
-        self._rep_policy.touch()
-        self._dirty = True
-
-    def is_dirty(self):
-        """
-        Returns if this block is written to, or dirty
-        :return: boolean, if this block is dirty
-        """
-        return self._dirty
+            # Block is not existing in cache and space is not available, evict
+            evicted_block_index = self._policy.evict(self._cache[cache_set])
+            evicted_block = self._cache[cache_set][evicted_block_index]
+            self._cache[cache_set][evicted_block_index] = block
+            return evicted_block
 
 
 class AddressSpace(enum.Enum):
