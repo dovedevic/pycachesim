bee8621d26ad4cbd741566a52ec7d31d65868624
dovedevic <dovedevic@gmail.com>
2019-10-13 17:40:49 -0400

    added latency data to all cache systems

--- hierarchies/three_level_suu_exclusive_bypassing_cache_system.py
+++ hierarchies/three_level_suu_exclusive_bypassing_cache_system.py
@@ -5,7 +5,7 @@ from policies.replacement_policies import BaseReplacementPolicy as ReplacementPo
 
 
 class ThreeLevelSUUExclusiveBypassingCacheSystem:
-    def __init__(self, space: AddressSpace, policy: ReplacementPolicy, level_sizes: list, level_associativites: list, blocksize):
+    def __init__(self, space: AddressSpace, policy: ReplacementPolicy, level_sizes: list, level_associativites: list, blocksize, level_latencies: list = None):
         self._space = space
         self._replacement_policy = policy
         self._blocksize = blocksize
@@ -16,11 +16,23 @@ class ThreeLevelSUUExclusiveBypassingCacheSystem:
         if not isinstance(level_associativites, list) or len(level_associativites) != 3:
             raise AttributeError("Field 'level_associativites' must be a list of length 3 indicating I/DL1, UL2, and UL3 associativity")
 
-        self.DL1 = Cache(space, level_sizes[0], level_associativites[0], blocksize, policy, name='DL1')
-        self.IL1 = Cache(space, level_sizes[0], level_associativites[0], blocksize, policy, name='IL1')
-        self.UL2 = Cache(space, level_sizes[1], level_associativites[1], blocksize, policy, name='UL2')
-        self.UL3 = Cache(space, level_sizes[2], level_associativites[2], blocksize, policy, name='UL3')
-        self.MEM = Cache(space, blocksize, 1, blocksize, policy, name='MEM')
+        if level_latencies:
+            if not isinstance(level_latencies, list) or len(level_latencies) != 4:
+                raise AttributeError("Field 'level_latencies' must be a list of length 4 indicating I/DL1, UL2, UL3, and MEM latencies")
+            for level in level_latencies:
+                if not isinstance(level, tuple) or len(level) != 2:
+                    raise AttributeError("Field 'level_latencies' must be a list of tuples indicating (read_latency, write_latency)")
+            self.DL1 = Cache(space, level_sizes[0], level_associativites[0], blocksize, policy, name='DL1', rlatency=level_latencies[0][0], wlatency=level_latencies[0][1])
+            self.IL1 = Cache(space, level_sizes[0], level_associativites[0], blocksize, policy, name='IL1', rlatency=level_latencies[0][0], wlatency=level_latencies[0][1])
+            self.UL2 = Cache(space, level_sizes[1], level_associativites[1], blocksize, policy, name='UL2', rlatency=level_latencies[1][0], wlatency=level_latencies[1][1])
+            self.UL3 = Cache(space, level_sizes[2], level_associativites[2], blocksize, policy, name='UL3', rlatency=level_latencies[2][0], wlatency=level_latencies[2][1])
+            self.MEM = Cache(space, blocksize, 1, blocksize, policy, name='MEM', rlatency=level_latencies[3][0], wlatency=level_latencies[3][1])
+        else:
+            self.DL1 = Cache(space, level_sizes[0], level_associativites[0], blocksize, policy, name='DL1')
+            self.IL1 = Cache(space, level_sizes[0], level_associativites[0], blocksize, policy, name='IL1')
+            self.UL2 = Cache(space, level_sizes[1], level_associativites[1], blocksize, policy, name='UL2')
+            self.UL3 = Cache(space, level_sizes[2], level_associativites[2], blocksize, policy, name='UL3')
+            self.MEM = Cache(space, blocksize, 1, blocksize, policy, name='MEM')
 
         self.stats = CacheMetrics(
             [self.IL1.name, self.DL1.name, self.UL2.name, self.UL3.name, self.MEM.name],
@@ -38,21 +50,25 @@ class ThreeLevelSUUExclusiveBypassingCacheSystem:
     def perform_fetch(self, address, for_data=True):
         cache = self.DL1 if for_data else self.IL1
         block = cache.get(address)
+        self.stats.add_latency(cache.read_latency, True)
         hit_in = cache
         if block is None:
             self.stats.add_miss(cache.name)
             cache = self.UL2
             block = cache.get(address)
+            self.stats.add_latency(cache.read_latency, True)
             hit_in = self.UL2
 
             if block is None:
                 self.stats.add_miss(cache.name)
                 cache = self.UL3
                 block = cache.get(address)
+                self.stats.add_latency(cache.read_latency, True)
                 hit_in = self.UL3
                 if block is None:
                     self.stats.add_miss(cache.name)
                     # Not in the cache, fetch from memory
+                    self.stats.add_latency(self.MEM.read_latency, True)
                     # Allocate new block from MEM to L3
                     block = Block(address & self.UL3.get_base_address_mask(), False, self.UL3.get_policy())
                     block.read()
@@ -98,21 +114,25 @@ class ThreeLevelSUUExclusiveBypassingCacheSystem:
     def perform_set(self, address, for_data=True):
         cache = self.DL1 if for_data else self.IL1
         block = cache.get(address)
+        self.stats.add_latency(cache.write_latency, False)
         hit_in = cache
         if block is None:
             self.stats.add_miss(cache.name)
             cache = self.UL2
             block = cache.get(address)
+            self.stats.add_latency(cache.write_latency, False)
             hit_in = self.UL2
 
             if block is None:
                 self.stats.add_miss(cache.name)
                 cache = self.UL3
                 block = cache.get(address)
+                self.stats.add_latency(cache.write_latency, False)
                 hit_in = self.UL3
                 if block is None:
                     self.stats.add_miss(cache.name)
                     # Not in the cache, fetch from memory
+                    self.stats.add_latency(self.MEM.write_latency, False)
                     # Allocate new block from MEM to L3
                     block = Block(address & self.UL3.get_base_address_mask(), False, self.UL3.get_policy())
                     block.write()
